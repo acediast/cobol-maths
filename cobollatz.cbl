b5f7b4520d42b236fcbd76de6245fc349db4bdf0
acediast <123393158+acediast@users.noreply.github.com>
2023-06-10 17:01:50 +0800

    Replace SECTION with user-defined function.

--- cobollatz.cbl
+++ cobollatz.cbl
@@ -1,32 +1,52 @@
-       Identification division.
-      
-       Program-ID. COBOLLATZ.
+000100 Identification division.
+       Program-ID.    COBOLLATZ.
+       Date-written.  2023-06-08.
+       Installation.  WSL2.
+       Security.      Non-confidential.
+
+       Environment division.
+       Configuration section. 
+       Source-computer. x86-64.
+       Repository.
+           Function HAILSTONE.
 
        Data division.
-
        Working-storage section.
-       01 WS-INTEGERS.
-           05 WS-START-PT  BINARY-LONG.
-           05 WS-ITERATION BINARY-LONG.
+       77 WS-NUMBER usage is binary-long.
+       77 WS-OUTPUT picture is Z(9)9.
 
        Procedure division.
-
        Main section.
-           Initialize WS-START-PT, WS-ITERATION.
-           Display "Input starting point:".
-           Accept WS-START-PT from console.
-           Move WS-START-PT to WS-ITERATION.
-           Perform GENERATE-COLLATZ
-               with test after until WS-ITERATION is equal to 1.
-           Display "END".
+           Initialize WS-NUMBER.
+           Display "Input starting point: " with no advancing.
+           Accept WS-NUMBER from console.
+           Perform with test after until WS-NUMBER is equal to 1
+               compute WS-NUMBER = HAILSTONE(WS-NUMBER)
+               move WS-NUMBER to WS-OUTPUT
+               display function trim(WS-OUTPUT)
+           end-perform.
+           Display "End.".
            Stop run.
 
-       GENERATE-COLLATZ section.
-           Evaluate function rem (WS-ITERATION,2)
+       End program COBOLLATZ.
+
+       Identification division.
+       Function-ID. HAILSTONE.
+       
+       Data division.
+       Linkage section.
+       77 LS-ARGUMENT usage is binary-long unsigned.
+       77 LS-RESULT usage is binary-long unsigned.
+
+       Procedure division using LS-ARGUMENT returning LS-RESULT.
+       Main section.
+           Evaluate function rem(LS-ARGUMENT,2)
                when 0
-                   divide WS-ITERATION by 2 giving WS-ITERATION
+                   divide LS-ARGUMENT by 2 giving LS-RESULT
                when not 0
-                   multiply WS-ITERATION by 3 giving WS-ITERATION
-                   add 1 to WS-ITERATION giving WS-ITERATION
-               end-evaluate.
-           Display WS-ITERATION.
+                   multiply LS-ARGUMENT by 3 giving LS-RESULT
+                   add 1 to LS-RESULT giving LS-RESULT
+           end-evaluate.
+           Exit function.
+
+       End function HAILSTONE.
