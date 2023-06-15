eef477bcf82cd8588550f3e7cd2f0f1bace95d4d
acediast <amirul.rudy@gmail.com>
2023-06-15 18:35:16 +0800

    Refactored with level-88s.

--- bmi/bmi.cbl
+++ bmi/bmi.cbl
@@ -11,6 +11,9 @@
 002000 Data division.
        Working-storage section.
        77 WS-BMI            picture is 99.9.
+           88 UNDERWEIGHT   value is 0 through 18.4.
+           88 ACCEPTABLE    value is 18.5 through 23.0.
+           88 OVERWEIGHT    value is 23.1 through 99.9.
        77 WS-WEIGHT         usage is float-short. 
        77 WS-HEIGHT         usage is float-short.
        77 WS-ACCEPTABILITY  picture is X(18).
@@ -24,21 +27,25 @@
            Perform calculate-BMI.
            Display "Body Mass Index:" space WS-BMI.
            Perform EVALUATE-ACCEPTABILITY.
-           Display WS-ACCEPTABILITY.
+           Display function trim(WS-ACCEPTABILITY).
            Stop run.
        CALCULATE-BMI section.
            Initialize WS-BMI.
-           Compute WS-BMI = WS-WEIGHT / (WS-HEIGHT ** 2)
+           Compute WS-BMI
+               rounded mode is nearest-away-from-zero
+               = WS-WEIGHT / (WS-HEIGHT ** 2)
            end-compute.
            Exit section.
        EVALUATE-ACCEPTABILITY section.
            Evaluate true
-               when WS-BMI is less than 18.5
+               when UNDERWEIGHT
                    move "Underweight." to WS-ACCEPTABILITY
-               when WS-BMI is greater than 23.0
+               when OVERWEIGHT
                    move "Overweight." to WS-ACCEPTABILITY
-               when other
+               when ACCEPTABLE
                    move "Acceptable weight." to WS-ACCEPTABILITY
+               when other
+                   continue
            end-evaluate.
            Exit section.
 
